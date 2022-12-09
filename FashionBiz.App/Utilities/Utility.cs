db5cba812ede2a7d37898435bff765750fd3a5e4
Azeez Adebayo <azeez.adedayo@yahoo.com>
2022-12-10 00:05:58 +0100

    Added utility class

---
+++ FashionBiz.App/Utilities/Utility.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace FashionBiz.App.Utilities
+{
+    public class Utility
+    {
+        public static string HashPassword(string password)
+        {
+            string result;
+            SHA1Managed sha1 = new SHA1Managed();
+            byte[] hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(password));
+            StringBuilder sb = new StringBuilder(hash.Length * 2);
+            foreach (byte b in hash)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+            result = sb.ToString();
+            return result;
+        }
+    }
+}
