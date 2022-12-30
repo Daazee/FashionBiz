12f9bdabf6b89ecf80e2241e6bfb2f2b9078af67
Azeez Adebayo <azeez.adedayo@yahoo.com>
2022-12-30 14:44:20 +0100

    Added Producs and Stocks pages

---
+++ FashionBiz.App/Pages/Product/Products.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FashionBiz.App.Pages.Product
+{
+    public class ProductsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ FashionBiz.App/Pages/Stock/Stocks.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FashionBiz.App.Pages.Stock
+{
+    public class StocksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
